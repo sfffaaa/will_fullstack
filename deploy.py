128b151d49c8ed115f6b4aa3609b45dff9d1e6eb
Jay Pan <sfffaaa@gmail.com>
2018-02-21 20:36:27 +0100

    Change function name and add log for indicate where process executes.

--- deploy.py
+++ deploy.py
@@ -10,21 +10,21 @@ import time
 CONFIG_PATH = 'etc/config.conf'
 
 
-def ComposeContractBuildPath(truffle_build_path, target_contract_name):
+def _ComposeContractBuildPath(truffle_build_path, target_contract_name):
     json_filename = '{0}.json'.format(target_contract_name)
     target_path = os.path.join(*[truffle_build_path, 'contracts', json_filename])
     return target_path
 
 
-def GetBuildContractJsonFileAttribute(filepath, key):
+def _GetBuildContractJsonFileAttribute(filepath, key):
     with open(filepath) as f:
         return json.load(f)[key]
 
 
-def DeploySmartContract(contract_path, file_ipc):
+def _DeploySmartContract(contract_path, file_ipc):
     w3 = Web3(Web3.IPCProvider(file_ipc))
-    abi = GetBuildContractJsonFileAttribute(contract_path, 'abi')
-    bytecode = GetBuildContractJsonFileAttribute(contract_path, 'bytecode')
+    abi = _GetBuildContractJsonFileAttribute(contract_path, 'abi')
+    bytecode = _GetBuildContractJsonFileAttribute(contract_path, 'bytecode')
     contract = w3.eth.contract(abi=abi, bytecode=bytecode)
     tx_hash = contract.deploy(transaction={'from': w3.eth.accounts[0], 'gas': 410000})
     tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
@@ -38,21 +38,19 @@ def DeploySmartContract(contract_path, file_ipc):
 
     w3.miner.stop()
     if not tx_receipt:
-        raise IOError('still cannot get ')
+        raise IOError('still cannot get contract result')
 
-    print(tx_receipt)
     return tx_receipt
 
 
-def DumpContractInfo(contract_path, contract_detail, file_path):
-    # file_path = os.path.abspath(file_path)
-    # print(file_path)
+def _DumpContractInfo(contract_path, contract_detail, file_path):
+    file_path = os.path.abspath(file_path)
     dir_path = os.path.dirname(file_path)
     if not os.path.isdir(dir_path):
         os.makedirs(dir_path)
 
     json_data = {
-        'abi': GetBuildContractJsonFileAttribute(contract_path, 'abi'),
+        'abi': _GetBuildContractJsonFileAttribute(contract_path, 'abi'),
         'address': contract_detail['contractAddress'],
         'detail': {k: v for k, v in contract_detail.items()}
     }
@@ -64,13 +62,21 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH)
 
-    truffle_build_path = config.get('Deploy', 'truffle_build_path')
-    target_contract_name = config.get('Deploy', 'target_contract_name')
-    contract_path = ComposeContractBuildPath(truffle_build_path, target_contract_name)
+    print('==== Deploy started ====')
+    contract_path = _ComposeContractBuildPath(config.get('Deploy', 'truffle_build_path'),
+                                              config.get('Deploy', 'target_contract_name'))
+
     assert os.path.isfile(contract_path), 'file path {0} doesn\'t exist'.format(contract_path)
 
-    file_ipc = config.get('Ethereum', 'file_ipc')
-    contract_detail = DeploySmartContract(contract_path, file_ipc)
+    print('==== Deploy contract to private chain  ====')
+    contract_detail = _DeploySmartContract(contract_path,
+                                           config.get('Ethereum', 'file_ipc'))
 
-    file_path = config.get('Output', 'file_path')
-    DumpContractInfo(contract_path, contract_detail, file_path)
+    _DumpContractInfo(contract_path,
+                      contract_detail,
+                      config.get('Output', 'file_path'))
+
+    print('==== Deploy finished ====')
+    print('Contract detail:')
+    for k, v in contract_detail.items():
+        print('    {0}: {1}'.format(k, v))
