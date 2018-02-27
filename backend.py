6cacf4b3a958a89a98242184b6db9544b2e16686
Jay Pan <sfffaaa@gmail.com>
2018-02-27 16:59:37 +0100

    Check the input for request

--- backend.py
+++ backend.py
@@ -6,7 +6,10 @@ import flask
 from utils import myeth_utils
 from utils import aes_utils
 from utils import onchain_utils
+from utils import req_utils
 import json
+import traceback
+import sys
 
 app = Flask(__name__)
 
@@ -24,9 +27,19 @@ def serve_static(filename):
 # Note the order is very important
 @app.route("/will/create", methods=['POST'])
 def create_will():
-    # [TODO] Check request, the num_sig shouldn't be zero
-    num_signature = int(flask.request.form.get('num_sig'))
+    num_signature = flask.request.form.get('num_sig')
     raw_data = flask.request.form.get('raw_data')
+    try:
+        req_utils.CheckCreateInput(num_signature, raw_data)
+    except Exception as e:
+        traceback.print_exc()
+        exc_tuple = sys.exc_info()
+        return json.dumps({
+            'success': False,
+            'error': str(exc_tuple[1]).strip()
+        })
+
+    num_signature = int(num_signature)
 
     my_private_key = myeth_utils.GenerateRandomPrivateKeyInBytes()
     private_keys = [myeth_utils.GenerateRandomPrivateKeyInBytes() for _ in range(num_signature)]
@@ -50,9 +63,18 @@ def create_will():
 def update_will():
     my_private_key = flask.request.form.get('my_private_key')
     raw_data = flask.request.form.get('raw_data')
-    other_private_keys_dict = json.loads(flask.request.form.get('others_private_key'))
-    other_private_keys = myeth_utils.SortedPrivateKeys(other_private_keys_dict, reverse=False)
-    other_private_keys = [bytes.fromhex(_) for _ in other_private_keys]
+    other_private_keys_dict = flask.request.form.get('others_private_key')
+    try:
+        req_utils.CheckUpdateInput(my_private_key, other_private_keys_dict, raw_data)
+    except Exception as e:
+        traceback.print_exc()
+        exc_tuple = sys.exc_info()
+        return json.dumps({
+            'success': False,
+            'error': str(exc_tuple[1]).strip()
+        })
+
+    other_private_keys = req_utils.ConvertOtherPrivateKeysStr(other_private_keys_dict)
 
     encrypt_data = str(raw_data)
     for enc_private_key in other_private_keys:
@@ -71,8 +93,17 @@ def update_will():
 
 @app.route("/will/delete", methods=['POST'])
 def delete_will():
-    # [TODO] Check request
     my_private_key = flask.request.form.get('my_private_key')
+    try:
+        req_utils.CheckDeleteInput(my_private_key)
+    except Exception as e:
+        traceback.print_exc()
+        exc_tuple = sys.exc_info()
+        return json.dumps({
+            'success': False,
+            'error': str(exc_tuple[1]).strip()
+        })
+
     onchain_utils.DeleteWillToOnchain(myeth_utils.PrivToAddr(my_private_key))
     return json.dumps({
         'success': True
@@ -81,8 +112,18 @@ def delete_will():
 
 @app.route("/will/retrieve", methods=['POST'])
 def retrieve_will():
-    # [TODO] Check request
     my_private_key = flask.request.form.get('my_private_key')
+    other_private_keys_dict = flask.request.form.get('others_private_key')
+    try:
+        req_utils.CheckRetrieveInput(my_private_key, other_private_keys_dict)
+    except Exception as e:
+        traceback.print_exc()
+        exc_tuple = sys.exc_info()
+        return json.dumps({
+            'success': False,
+            'error': str(exc_tuple[1]).strip()
+        })
+
     encrypt_data = onchain_utils.RetrieveWillToOnchain(myeth_utils.PrivToAddr(my_private_key))
 
     if not encrypt_data:
@@ -91,10 +132,7 @@ def retrieve_will():
             'raw_data': ''
         })
 
-    other_private_keys_dict = json.loads(flask.request.form.get('others_private_key'))
-
-    other_private_keys = myeth_utils.SortedPrivateKeys(other_private_keys_dict)
-    other_private_keys = [bytes.fromhex(_) for _ in other_private_keys]
+    other_private_keys = req_utils.ConvertOtherPrivateKeysStr(other_private_keys_dict)
 
     for decrypt_private_key in other_private_keys:
         encrypt_data = aes_utils.AESDecrypt(decrypt_private_key, encrypt_data)
